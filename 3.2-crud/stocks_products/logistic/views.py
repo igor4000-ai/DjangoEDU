e9216c4be6ef18edeaaeda42f8d90cd2f15351a6
igor4000-ai <igor4000-ai@users.noreply.github.com>
2026-07-26 07:09:04 +0300

    stocks_products: implement CRUD, search, pagination for Products and Stocks

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from django.db.models import Q
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -7,10 +8,27 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        search = self.request.query_params.get('search')
+        if search:
+            queryset = queryset.filter(
+                Q(title__icontains=search) | Q(description__icontains=search)
+            )
+        return queryset
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+
+    def get_queryset(self):
+        queryset = Stock.objects.all()
+        products = self.request.query_params.get('products')
+        if products:
+            queryset = queryset.filter(positions__product_id=products)
+        search = self.request.query_params.get('search')
+        if search:
+            queryset = queryset.filter(positions__product__title__icontains=search)
+        return queryset.distinct()
